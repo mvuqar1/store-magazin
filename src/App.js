4248959e8aa571db2c4dbed98fabf78ef09fcf58
mvuqar1 <vuqar.5100@mail.ru>
2023-05-16 18:01:15 +0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import { Home,About,Products,SingleProducts,Cart,Error,Checkout,Private } from './pages'
+
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route exact path="/about"><About /></Route>
+        <Route exact path="/products"><Products /></Route>
+        
+        <Route exact path="/cart"><Cart /></Route>
+        <Route exact path="/products/:id" children={<SingleProducts />}> </Route>
+        <Route exact path="/checkout"><Checkout /></Route>
+        <Route path="*"><Error /></Route>
+      </Switch>
+      <Footer />
+    </Router>
+  
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage"
+import About from "./AboutPage"
+import Products from "./ProductsPage"
+import SingleProducts from "./SingleProductPage"
+import Cart from "./CartPage"
+import Error from "./ErrorPage"
+import Checkout from "./CheckoutPage"
+import Private from "./PrivateRoute"
+
+export{
+    Home,
+    About,
+    Cart,
+    Products,
+    SingleProducts,
+    Checkout,
+    Error,
+    Private
+}
