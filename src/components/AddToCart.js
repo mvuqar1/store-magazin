51c603cf033f09f40eacd6248dea317f84e0965d
mvuqar1 <vuqar.5100@mail.ru>
2023-05-26 17:42:40 +0400

    442

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
@@ -6,21 +6,22 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext()
   const { id, stock, colors } = product
   const [mainColor, setMainColor] = useState(colors[0])
   const [amount, setAmount] = useState(1)
 
-  const increase=()=>{
-    if(stock===amount){
+  const increase = () => {
+    if (stock === amount) {
       return amount
     }
-    setAmount(prevstate=>prevstate+1)
+    setAmount(prevstate => prevstate + 1)
   }
-  const decrease=()=>{
-    setAmount((prevstate)=>{
-      let tempAmount=prevstate-1
-      if(tempAmount<1){
-        tempAmount=1
+  const decrease = () => {
+    setAmount((prevstate) => {
+      let tempAmount = prevstate - 1
+      if (tempAmount < 1) {
+        tempAmount = 1
       }
       return tempAmount
     })
@@ -43,8 +44,12 @@ const AddToCart = ({ product }) => {
         </div>
       </div>
       <div className="btn-container">
-        <AmountButtons amount={amount} increase={increase} decrease={decrease}/>
-        <Link to="/cart" className='btn'>
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
+        <Link to="/cart" className='btn' onClick={() => addToCart(id, mainColor, amount, product)}>
           add to cart
         </Link>
       </div>
